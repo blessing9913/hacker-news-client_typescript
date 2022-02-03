045ab4927f2f5d1eea22159c128a372b37ea3e96
blessing9913 <blessing9913@gmail.com>
2022-02-03 20:52:49 +0900

    TypeScript 적용

--- app.ts
+++ app.ts
@@ -1,19 +1,49 @@
-const ajax = new XMLHttpRequest();
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[]
+}
+
+type News = {
+  id: number;
+  time_ago: string;
+  title: string;
+  url: string;
+  user: string;
+  content: string;
+}
+
+type NewsFeed = News & {
+  comments_count: number;
+  points: number;
+  read?: boolean;
+}
+
+type NewsDetail = News & {
+  comments: NewsComment[];
+}
+
+type NewsComment = News & {
+  comments: NewsComment[];
+  level: number;
+}
+
+const container: HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_RUL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 }
 
-function getData(url) {
+function getData<APIResponse>(url: string): APIResponse {
   ajax.open('GET', url, false); // async = false 동기처리
   ajax.send();
 
   return JSON.parse(ajax.response);
 }
 
-function makeFeeds(feeds) {
+function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
   for (let i = 0; i < feeds.length; i++) {
     feeds[i].read = false;
   }
@@ -21,10 +51,18 @@ function makeFeeds(feeds) {
   return feeds;
 }
 
+function updateView(html: string): void {
+  if (container) {
+    container.innerHTML = html;
+  } else {
+    console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.');
+  }
+}
+
 // 글 목록
-function newsFeed() {
+function newsFeed(): void {
   // const newsFeed = getData(NEWS_URL);
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -48,7 +86,7 @@ function newsFeed() {
   `;
 
   if (newsFeed.length === 0) {
-    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
   }
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -74,19 +112,18 @@ function newsFeed() {
   }
 
   template = template.replace('{{__news_feed__}}', newsList.join(''));
-  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
-  template = template.replace('{{__next_page__}}', newsFeed.length > store.currentPage * 10 ? store.currentPage + 1 : store.currentPage);
+  template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
+  template = template.replace('{{__next_page__}}', String(newsFeed.length > store.currentPage * 10 ? store.currentPage + 1 : store.currentPage));
 
-  container.innerHTML = template;
+  updateView(template);
 }
 
-const container = document.getElementById('root');
 const ul = document.createElement('ul');
 
 // 글 상세
 function newsDetail() {
   const id = location.hash.substr(7);
-  const newsContent = getData(CONTENT_RUL.replace('@id', id));
+  const newsContent = getData<NewsDetail>(CONTENT_RUL.replace('@id', id));
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -123,47 +160,43 @@ function newsDetail() {
     }
   }
 
-  function regEx(content) {
-    content = content.replace(/\$/g, "&#36;");
+  updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+}
 
-    return content;
-  }
+function makeComment(comments: NewsComment[]): string {
+  const commentString = [];
 
-  function makeComment(comments, called = 0) {
-    const commentString = [];
+  for (let i = 0; i < comments.length; i++) {
+    const comment: NewsComment = comments[i];
 
-    for (let i = 0; i < comments.length; i++) {
-      if (comments[i].comments.length > 0) { // 삭제된 글은 미표시
-        commentString.push(`
-          <div style="padding-left: ${called * 40}px;" class="mt-4">
-            <div class="text-gray-400">
-              <i class="fa fa-sort-up mr-2"></i>
-              <strong>${comments[i].user}</strong> ${comments[i].time_ago}
-            </div>
-            <p class="text-gray-700">${regEx(comments[i].content)}</p>
+    if (comment.comments.length > 0) { // 삭제된 글은 미표시
+      commentString.push(`
+        <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comment.user}</strong> ${comment.time_ago}
           </div>
-        `);
-  
-        if (comments[i].comments.length > 0) {
-          commentString.push(makeComment(comments[i].comments, called + 1));
-        }
+          <p class="text-gray-700">${comment.content.replace(/\$/g, "&#36;")}</p>
+        </div>
+      `);
+
+      if (comment.comments.length > 0) {
+        commentString.push(makeComment(comment.comments));
       }
     }
-
-    return commentString.join('');
   }
 
-  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+  return commentString.join('');
 }
 
 // 라우터
-function router() {
+function router(): void {
   const routePath = location.hash;
 
   if(routePath === '') { // location.hash 에 #만 있을 경우 빈 값을 반환 함
     newsFeed();
   } else if(routePath.indexOf('/page/') >= 0) {
-    store.currentPage = Number(routePath.substr(7));
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
